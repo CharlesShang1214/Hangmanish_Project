0a934681a57bbb01545ae4ecd88e3ca899fc2d9a
@251 <144711618+CharlesShang1214@users.noreply.github.com>
2024-12-05 17:31:01 +0000

    phraseGetter Amazing work

--- PharseGetter.java
+++ PharseGetter.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Random;
 
 //https://www.geeksforgeeks.org/files-class-readstring-method-in-java-with-examples/
 public class PharseGetter {
@@ -11,12 +12,26 @@ public class PharseGetter {
 
         {
             String content = Files.readString(filePath);
-            int num = (int) (Math.random() * 98) + 1;
-            String dotNum = String.valueOf(num) + ". ";
-            String pharse = content.substring(content.indexOf(dotNum), content.indexOf(":"));
+            // // int num = (int) ((Math.random() * 98) + 1);
+            // // math.random doesnt work for some reason
+            Random rand = new Random();
+            int num = rand.nextInt((98) + 1) + 1;
+
+            String dottedNum = String.valueOf(num) + ". ";
+            String dottedNumPlusOne = String.valueOf(num + 1) + ". ";
+            String pharse = content.substring(content.indexOf(dottedNum), content.indexOf(dottedNumPlusOne));
+            String phraseDefinition = pharse.substring(pharse.indexOf(".") + 1);
+            String actualPharse = pharse.substring(pharse.indexOf(".") + 1, pharse.indexOf(":"));
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            // https://stackoverflow.com/questions/2979383/how-to-clear-the-console-using-java
             System.out.println(pharse);
+            System.out.println(actualPharse);
+            System.out.println(phraseDefinition);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
